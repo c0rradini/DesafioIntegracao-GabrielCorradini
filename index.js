59395dcc737583a031f990780f535d6291422da6
c0rradini <g.siqueira@edu.unipar.br>
2022-05-25 17:12:30 -0300

    Adiciona script para puxar do Google Sheets via API e Adicionar no HubSpot via API.
    Erro: Apresenta erro ao tentar incluir os mesmos, não valida se já existe.

--- index.js
+++ index.js
@@ -1,14 +1,64 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const hubspot = require('@hubspot/api-client')
-const {google} = require('googleapis');
+const {google} = require('googleapis')
 const sheets = google.sheets('v4');
+const hubspot = require('@hubspot/api-client')
 
-app.get('/sincroniza', (req, res) => {
-    res.send('Hello World!')
-    const hubspotClient = new hubspot.Client({ apiKey: process.env.YOUR_API_KEY })
+
+app.get('/sync', async (req, res) => {
+
+    const hubspotClient = new hubspot.Client({apiKey: process.env.API_HUBSPOT})
+
+    const auth = new google.auth.GoogleAuth
+    ({keyFile: 'credentials.json', scopes: 'https://www.googleapis.com/auth/spreadsheets'})
+
+    const client1 = await auth.getClient()
+    const googleSheet = google.sheets({version: 'v4', auth: client1})
+    const spreadsheetId = process.env.ID_SHEETS
+
+    const range = 'A:E'
+
+    const sheetData = {
+        auth,
+        spreadsheetId,
+        range
+    }
+
+    let mapArray
+
+    const getSheetData = await googleSheet.spreadsheets.values.get(sheetData).then(function (result) {
+
+        mapArray = result.data.values.map(function (item) {
+            //console.log(item)
+            return {
+                "properties": {
+                    "company": item[0],
+                    "firstname": item[1],
+                    "email": item[2],
+                    "phone": item[3],
+                    "website": item[4]
+                }
+            }
+        })
+    })
+
+    const BatchInputSimplePublicObjectInput = {
+        inputs: mapArray
+    };
+
+    try {
+        const apiResponse = await hubspotClient.crm.contacts.batchApi.create(BatchInputSimplePublicObjectInput);
+        res.send(apiResponse.body);
+    } catch (e) {
+        res.send(e.body);
+
+        e.message === 'HTTP request failed'
+            ? console.error(JSON.stringify(e.response, null, 2))
+            : console.error(e)
+   }
 
 })
 
 app.listen(process.env.PORT)
+
