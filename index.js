db9648d0adece07973fbdc08c48e6f878f0c418f
c0rradini <g.siqueira@edu.unipar.br>
2022-05-26 15:21:28 -0300

    Adiciona validação de e-mail corporativo.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const {google} = require('googleapis')
 const sheets = google.sheets('v4');
 const hubspot = require('@hubspot/api-client')
+const url = require('url');
 
 
 app.get('/sync', async (req, res) => {
@@ -37,21 +38,39 @@ app.get('/sync', async (req, res) => {
                 "company": item[0], "firstname": item[1], "email": item[2], "phone": item[3], "website": item[4]
             }
             // console.log(properties)
-            try {
-                const apiResponse = await hubspotClient.crm.contacts.basicApi.create({properties});
-                //res.send(apiResponse.body)
-                //console.log(item[1] + " Cadastrado.")
-                cadastrados.push(item[1])
 
-            } catch (e) {
-                //console.log(item[1] + " Não cadastrado.")
-                naoCadastrados.push(item[1])
-                // res.send(e.body)
-                // e.message === 'HTTP request failed'
-                //     ? console.error(JSON.stringify(e.response, null, 2))
-                //     : console.error(e)
+            // Pegar item[4] parseURL dps de www.
+            // Pegar item[2] parse dps do @+1 e comparar com o parseURL do item[4]
+            // se for igual, cadastra, se não, erro.
+
+            const email_hub = item[2]
+            const email_hut_whost = email_hub.substring(email_hub.indexOf('@') + 1)
+            //console.log(email_hut_whost)
+
+            const website_hub = item[4]
+            const website_hub_whost = website_hub.substring(website_hub.indexOf('') + 4)
+            //console.log(website_hub_whost)
+
+            if (email_hut_whost === website_hub_whost) {
+
+                try {
+                    const apiResponse = await hubspotClient.crm.contacts.basicApi.create({properties});
+                    //res.send(apiResponse.body)
+                    console.log(item[1] + " Cadastrado.")
+                    //cadastrados.push(item[1])
+
+                } catch (e) {
+                    console.log(item[1] + " Já cadastrado.")
+                    // naoCadastrados.push(item[1])
+                    // res.send(e.body)
+                    // e.message === 'HTTP request failed'
+                    //     ? console.error(JSON.stringify(e.response, null, 2))
+                    //     : console.error(e)
+                }
+
+            } else {
+                console.log(item[2] + " Não foi possível cadastrar pois o e-mail não é da sua corporação.")
             }
-
         })
 
     })
