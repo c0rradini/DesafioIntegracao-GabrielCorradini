4309709aeba84051d3880723435539159bfd56eb
c0rradini <g.siqueira@edu.unipar.br>
2022-05-27 19:35:21 -0300

    Adiciona teste.

---
+++ index.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("./index");
+
+describe("Teste para sincronização.", () => {
+    test("Testa se Google Sheets e HubSpot estão sincronizando.", async () => {
+        const response = await request(app).get("/sync");
+        expect(response.statusCode).toBe(200);
+    });
+});
+
